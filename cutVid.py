89c0bbefb2ccb6a5cc36249cd75c0849d697b169
GalaxyGamingBoy <xrteach@gmail.com>
2022-06-16 20:14:37 +0300

    Refactoring code to allow calling cut function on another file

--- cutVid.py
+++ cutVid.py
@@ -1,13 +1,13 @@
 import sys
 import os
 
-def runCMD(cmd):
-    os.system(cmd)
-
 commandArgs = sys.argv
 
+def cutVidFFMPEG(startTime, endTime, inputFile, outputFile):
+    return cmdRunStr = "ffmpeg -ss " + startTime + " -to " + endTime + " -i " + inputFile + " -c copy " + outputFile
+
 try:
-    cmdRunStr = "ffmpeg -ss " + commandArgs[1] + " -to " + commandArgs[2] + " -i " + commandArgs[3] + " -c copy " + commandArgs[4]
-    runCMD(cmdRunStr)
+    cmdRunStr = cutVidFFMPEG(commandArgs[1], commandArgs[2], commandArgs[3], commandArgs[4])
+    os.system(cmdRunStr)
 except:
     print("Usage: cutVid.py START_TIME(00:00:00) END_TIME(00:10:00) INPUT_FILE(cut.mp4) OUTPUT_FILE(cutOut.mp4)")
