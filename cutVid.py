034c1642207a59fba873e8e7c75c9bd0ff08cd4b
GalaxyGamingBoy <53054461+GalaxyGamingBoy@users.noreply.github.com>
2022-06-16 16:40:46 +0300

    Added Usage When Not Entering Parameters

--- cutVid.py
+++ cutVid.py
@@ -6,5 +6,8 @@ def runCMD(cmd):
 
 commandArgs = sys.argv
 
-cmdRunStr = "ffmpeg -ss " + commandArgs[1] + " -to " + commandArgs[2] + " -i " + commandArgs[3] + " -c copy " + commandArgs[4]
-runCMD(cmdRunStr)
+try:
+    cmdRunStr = "ffmpeg -ss " + commandArgs[1] + " -to " + commandArgs[2] + " -i " + commandArgs[3] + " -c copy " + commandArgs[4]
+    runCMD(cmdRunStr)
+except:
+    print("Usage: cutVid.py START_TIME(00:00:00) END_TIME(00:10:00) INPUT_FILE(cut.mp4) OUTPUT_FILE(cutOut.mp4)")
