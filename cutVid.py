1b3f3a12ce479d9f1980b3a41c7258a89cb61274
GalaxyGamingBoy <xrteach@gmail.com>
2022-06-19 20:27:54 +0300

    Refactored code

--- cutVid.py
+++ cutVid.py
@@ -3,11 +3,17 @@ import os
 
 commandArgs = sys.argv
 
-def cutVidFFMPEG(startTime, endTime, inputFile, outputFile):
-    return cmdRunStr = "ffmpeg -ss " + startTime + " -to " + endTime + " -i " + inputFile + " -c copy " + outputFile
+class FFMPEG_CutVideo:
+    def __init__(self, inputFile, outputFile, startTime, endTime):
+        self.inputFile = inputFile
+        self.outputFile = outputFile
+        self.startTime = startTime
+        self.endTime = endTime
+
+    def cut(self):
+        return f"ffmpeg -ss {self.startTime} -to {self.endTime} -i {self.inputFile} -c copy {self.outputFile}"
 
 try:
-    cmdRunStr = cutVidFFMPEG(commandArgs[1], commandArgs[2], commandArgs[3], commandArgs[4])
-    os.system(cmdRunStr)
+    os.system(FFMPEG_CutVideo(commandArgs[1], commandArgs[2], commandArgs[3], commandArgs[4]).cut())
 except:
     print("Usage: cutVid.py START_TIME(00:00:00) END_TIME(00:10:00) INPUT_FILE(cut.mp4) OUTPUT_FILE(cutOut.mp4)")
